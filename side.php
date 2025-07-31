fe2e565d2e35446741c6f60c7190dfa73fb9c47c
napa708 <68319010008@tatc.ac.th>
2025-07-31 11:24:24 +0700

    Add files via upload

---
+++ side.php
@@ -0,0 +1,41 @@
+ <div class="sidebar-wrapper">
+          <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+              class="nav sidebar-menu flex-column"
+              data-lte-toggle="treeview"
+              role="navigation"
+              aria-label="Main navigation"
+              data-accordion="false"
+              id="navigation"
+            >
+              <li class="nav-item">
+                <a href="index.php?page=dashboard" class="nav-link">
+                  <i class="nav-icon bi bi-speedometer"></i>
+                  <p>Dashboard</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="index.php?page=users" class="nav-link">
+                  <i class="nav-icon bi bi-people"></i>
+                  <p>User</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="../logout.php" class="nav-link">
+                  <i class="bi bi-box-arrow-in-right"></i>
+                  <p>logout</p>
+                </a>
+              </li>
+             
+             
+             
+              
+             
+   
+            </ul>
+            <!--end::Sidebar Menu-->
+          </nav>
+        </div>
---
+++ users.php
@@ -0,0 +1,95 @@
+<?php
+require '../connect.php';
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+?>
+
+ <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6">
+                <h3 class="mb-0">User_list</h3>
+              </div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">user_list</li>
+                </ol>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--begin::App Content-->
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->       
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card mb-4">
+                  <div class="card-header bg-primary text-white">
+                    <h3 class="card-title">User</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                    <table class="table table-bordered">
+                      <thead>
+                        <tr>
+
+                          <th>Username</th>
+                          <th>Full Name</th>
+                          <th>Email</th>
+                          <th>Phone</th>
+
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+                          <tr>
+                            <td><?php echo $row['username']; ?></td>
+                            <td><?php echo $row['fullname']; ?></td>
+                            <td><?php echo $row['email']; ?></td>
+                            <td><?php echo $row['phone']; ?></td>
+                          </tr>
+                        <?php endwhile; ?>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer clearfix">
+                    <ul class="pagination pagination-sm m-0 float-end">
+                      <li class="page-item">
+                        <a class="page-link" href="#">&laquo;</a>
+                      </li>
+                      <li class="page-item">
+                        <a class="page-link" href="#">1</a>
+                      </li>
+                      <li class="page-item">
+                        <a class="page-link" href="#">2</a>
+                      </li>
+                      <li class="page-item">
+                        <a class="page-link" href="#">3</a>
+                      </li>
+                      <li class="page-item">
+                        <a class="page-link" href="#">&raquo;</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <!-- /.card -->
+
+                
+                <!-- /.card -->
+              </div>
+              <!-- /.col -->
+              
+              <!-- /.col -->
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
